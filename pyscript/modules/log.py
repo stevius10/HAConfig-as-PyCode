6ef25cfa79316ceb013146bb3d59d5881964b3d0
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-02 16:41:02 +0100

    auto commit

--- pyscript/modules/log.py
+++ pyscript/modules/log.py
@@ -4,17 +4,22 @@ import logging
 import os
 
 class Log:
-    def __init__(self, logfile=pyscript.get_global_ctx()):
-        self.logfile = os.path.join(PATH_LOGS, logfile)
-        self.__setup()
-
-    def __call__(self, message):
-        self.log(self, message)
+  
+  def __init__(self, logfile=pyscript.get_global_ctx()):
+    self.logfile = logfile
+    self.logpath = os.path.join(PATH_LOGS, logfile)
     
-    def log(self, message):
-      if message: 
-        log.info(message)
+    logging.basicConfig(filename=self.logpath,
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+    self.logger = logging.getLogger(self.logfile)
 
-    def __setup(self):
-      os.remove(self.logfile)
-      logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)], force=True)
+  def __call__(self, message):
+    self.log(message)
+  
+  def log(self, message):
+    if not message: 
+      message = ""
+    self.logger.info(message)
--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -4,35 +4,29 @@ from log import Log
 import subprocess
 
 @service
-def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY, config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-    git_sync(repo_url, branch_name, credentials_path, key_path, config_path, commit_message)
-  
-def git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY, config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
+def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY,config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
     log = Log(pyscript.get_global_ctx())
     try:
-        result = subprocess.run(f"git config --local include.path '{config_path}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        log(result.stdout)
-        log(result.stderr)
+        config_output = subprocess.run(f"git config --local include.path '{config_path}'", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(config_output.stdout)
+        log.log(config_output.stderr)
 
-        result = subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        log(result.stdout)
-        log(result.stderr)
+        ssh_output = subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(ssh_output.stdout)
+        log.log(ssh_output.stderr)
 
-        result = subprocess.run(["git", "add", "."], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        log(result.stdout)
-        log(result.stderr)
+        add_output = subprocess.run(["git", "add", "."], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(add_output.stdout)
+        log.log(add_output.stderr)
 
-        result = subprocess.run(["git", "commit", "-m", commit_message], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        log(result.stdout)
-        log(result.stderr)
+        commit_output = subprocess.run(["git", "commit", "-m", commit_message], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(commit_output.stdout)
+        log.log(commit_output.stderr)
 
-        result = subprocess.run(["git", "push", "origin", branch_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        log(result.stdout)
-        log(result.stderr)
-        
-        result = subprocess.run(["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        log(result.stdout)
-        log(result.stderr)
-        
+        # subprocess.run(["git", "push", "origin", branch_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # merge_request_command = ["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"]
+        # subprocess.run(merge_request_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     except subprocess.CalledProcessError as e:
         log(f"Error: {e}")
