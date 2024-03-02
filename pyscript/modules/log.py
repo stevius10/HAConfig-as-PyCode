4e28e5dc3e1f6e036b897a42e241f577efbafc73
stevius10 <steven.johann.koch@googlemail.com>
2024-03-02 12:20:06 +0100

    auto commit

--- pyscript/modules/log.py
+++ pyscript/modules/log.py
@@ -4,18 +4,13 @@ import logging
 import os
 
 class Log:
+    def __init__(self, ctx, logfile="logfile.log"):
+        self.logfile = os.path.join(PATH_LOGS, logfile)
+        self.__setup()
 
-  def __init__(self, logfile):
-      self.logfile = logfile
-      self.logpath = f"{PATH_LOGS}{logfile}"
-      self.__setup()
+    def __call__(self, message):
+        print(message)
+        logging.info(message)
 
-  def __call__(self, message):
-    print(message)
-    logging.info(message)
-
-  def __setup(self):
-    # if os.path.exists(self.logpath):
-      # os.remove(self.logpath)
-
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logpath)])
+    def __setup(self):
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)])
