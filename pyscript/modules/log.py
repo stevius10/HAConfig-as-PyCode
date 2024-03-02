fa2e97658bea006ab3283482617fcf5758d10317
stevius10 <steven.johann.koch@googlemail.com>
2024-03-02 12:28:46 +0100

    auto commit

--- pyscript/modules/log.py
+++ pyscript/modules/log.py
@@ -4,16 +4,13 @@ import logging
 import os
 
 class Log:
-    def __init__(self, ctx, logfile="logfile.log"):
+    def __init__(self, logfile=pyscript.get_global_ctx()):
         self.logfile = os.path.join(PATH_LOGS, logfile)
         self.__setup()
 
     def __call__(self, message):
         print(message)
-        logging.warn("xxx")
-        logging.error("testerei")
         logging.info(message)
 
     def __setup(self):
-      logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)])
-      logging.error("testerei")
+      logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)], force=True)
--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -5,7 +5,6 @@ import subprocess
 
 @service
 def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY,config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-  print(pyscript.get_global_ctx())
   log = Log(pyscript.get_global_ctx())
   try:
     
