58c5626d6bd2787f4cb24fd6dadef4d361eea93e
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-02 13:26:07 +0100

    auto commit

--- pyscript/modules/log.py
+++ pyscript/modules/log.py
@@ -9,13 +9,11 @@ class Log:
         self.__setup()
 
     def __call__(self, message):
-        log(self, message)
+        self.log(self, message)
     
     def log(self, message):
-      print(message)
-      logging.info(message)
       log.info(message)
 
     def __setup(self):
-      logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)], force=True)
-
+      os.remove(self.logfile)
+      logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(self.logfile)], force=True)
--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -5,20 +5,28 @@ import subprocess
 
 @service
 def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY,config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-  log = Log(pyscript.get_global_ctx())
-  try:
-    
-    log.log("test")
-    subprocess.run(f"git config --local include.path '$(config_path)'", shell=True, check=True)
+    log = Log(pyscript.get_global_ctx())
+    try:
+        config_output = subprocess.run(f"git config --local include.path '{config_path}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(config_output.stdout)
+        log.log(config_output.stderr)
 
-    subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, check=True)
-    subprocess.run(["git", "add", "."], check=True)
-    subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        ssh_output = subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(ssh_output.stdout)
+        log.log(ssh_output.stderr)
 
-    # subprocess.run(["git", "push", "origin", branch_name], check=True)
+        add_output = subprocess.run(["git", "add", "."], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(add_output.stdout)
+        log.log(add_output.stderr)
 
-    # merge_request_command = ["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"]
-    # subprocess.run(merge_request_command, check=True)
+        commit_output = subprocess.run(["git", "commit", "-m", commit_message], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        log.log(commit_output.stdout)
+        log.log(commit_output.stderr)
 
-  except subprocess.CalledProcessError as e:
-    log(f"Error: {e}")
+        # subprocess.run(["git", "push", "origin", branch_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # merge_request_command = ["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"]
+        # subprocess.run(merge_request_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    except subprocess.CalledProcessError as e:
+        log.log(f"Error: {e}")
