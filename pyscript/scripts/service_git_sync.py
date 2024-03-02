07c21f954d4d715bc1a3adf1702694bb4abbc932
stevius10 <steven.johann.koch@googlemail.com>
2024-03-02 12:31:48 +0100

    auto commit

--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -5,7 +5,10 @@ import subprocess
 
 @service
 def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY,config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-  log = Log(pyscript.get_global_ctx())
+  name = pyscript.get_global_ctx()
+  
+  log = Log(name)
+  log(name)
   try:
     
     log("test")
