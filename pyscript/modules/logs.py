fac75ab997e982928a937a0b8a5811f1c202f168
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-03 09:48:14 +0100

    auto commit

--- pyscript/modules/logs.py
+++ pyscript/modules/logs.py
@@ -4,16 +4,23 @@ import logging
 import os
 
 class Logs:
-    def __init__(self, name):
-        self.name = name.replace("scripts.", "")
-        logpath = os.path.join(PATH_LOGS, self.name) + ".log"
-        self.logger = logging.getLogger(self.name)
-        self.logger.addHandler(logging.handlers.WatchedFileHandler(logpath, mode='w+'))
-
-    def __call__(self, message):
-      if self.logger:
-        self.log(message)
+  
+  format = logging.Formatter('%(asctime)s %(funcName)s:%(lineno)d %(message)s')
+  
+  def __init__(self, name):
+    self.name = name.replace("scripts.", "")
+    logpath = os.path.join(PATH_LOGS, self.name) + ".log"
+    self.logger = logging.getLogger(self.name)
     
-    def log(self, message):
-      if message: 
-        self.logger.info(message)
+    handler = logging.handlers.WatchedFileHandler(logpath, mode='w+')
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(format)
+    self.logger.addHandler(handler)
+
+  def __call__(self, message):
+    if self.logger:
+      self.log(message)
+  
+  def log(self, message):
+    if message: 
+      self.logger.info(message)
