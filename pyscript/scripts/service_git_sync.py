9ecb2184c2b58aca6957410b454dac6b611f7d73
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-03 11:21:58 +0100

    auto commit

--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -5,33 +5,20 @@ import subprocess
 
 @service
 def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY, config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-  log = Log(pyscript.get_global_ctx())
-  log.log("test")
+  logs = Log(pyscript.get_global_ctx())
+
   try:
     result = subprocess.run(f"git config --local include.path '{config_path}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    log.log(result)
-  except subprocess.CalledProcessError as e:
-    log.log(e)
+    logs.log([result.stdout, result.stderr])
 
-  try:
     result = subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    log.log(result.stdout)
-    log.log(result.stderr)
-  except subprocess.CalledProcessError as e:
-    log.log(e)
-  
-  try:
+    logs.log([result.stdout, result.stderr])
+
     result = subprocess.run(["git", "add", "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    log.log(result.stdout)
-    log.log(result.stderr)
+    logs.log([result.stdout, result.stderr])
 
-  except subprocess.CalledProcessError as e:
-    log.log(e)
-
-  try:
     result = subprocess.run(["git", "commit", "-m", commit_message], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    log.log(result.stdout)
-    log.log(result.stderr)
+    log.log([result.stdout, result.stderr])
 
   except subprocess.CalledProcessError as e:
     log.log(e)
@@ -40,5 +27,5 @@ def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credent
 
       # merge_request_command = ["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"]
       # subprocess.run(merge_request_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-  return log.finished()
+  finally: 
+    return log.finished()
