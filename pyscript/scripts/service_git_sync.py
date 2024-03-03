82862ecece1c8c15511a108ab4d697605b8008f8
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-03 11:48:09 +0100

    auto commit

--- pyscript/scripts/service_git_sync.py
+++ pyscript/scripts/service_git_sync.py
@@ -5,27 +5,27 @@ import subprocess
 
 @service
 def service_git_sync(repo_url=GIT_REPO_URL, branch_name=GIT_BRANCH_NAME, credentials_path=GIT_CREDENTIALS_PATH, key_path=GIT_CREDENTIALS_KEY, config_path=GIT_CREDENTIALS_CONFIG, commit_message=GIT_COMMIT_MESSAGE):
-  logs = Log(pyscript.get_global_ctx())
+  util = Log(pyscript.get_global_ctx())
 
   try:
     result = subprocess.run(f"git config --local include.path '{config_path}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    logs.log([result.stdout, result.stderr])
+    util.log([result.stdout, result.stderr])
 
     result = subprocess.run(f"eval $(ssh-agent); ssh-add {key_path}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    logs.log([result.stdout, result.stderr])
+    util.log([result.stdout, result.stderr])
 
     result = subprocess.run(["git", "add", "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    logs.log([result.stdout, result.stderr])
+    util.log([result.stdout, result.stderr])
 
     result = subprocess.run(["git", "commit", "-m", commit_message], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    logs.log([result.stdout, result.stderr])
+    util.log([result.stdout, result.stderr])
 
   except subprocess.CalledProcessError as e:
-    logs.log(e)
+    util.log(e)
   
       # subprocess.run(["git", "push", "origin", branch_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
       # merge_request_command = ["git", "push", "-o", "merge_request.create", "-o", "merge_request.target=develop"]
       # subprocess.run(merge_request_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   finally: 
-    return logs.finished()
+    return util.finished()
