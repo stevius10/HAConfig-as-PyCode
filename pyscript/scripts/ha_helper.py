e06624aa436d5c2fdb2f8d55435f6c98d90af884
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-05 14:30:38 +0100

    auto/sync

--- pyscript/scripts/ha_helper.py
+++ pyscript/scripts/ha_helper.py
@@ -40,7 +40,7 @@ def log_truncate(trigger_type=None, log_file=PATH_LOG_HA, size_log_entries=SIZE_
     log_trunc = log_content[-size_log_entries:]
     async with aopen(log_file, 'w') as log_file_object:
       await log_file_object.writelines(log_trunc)
-      await log_file_object.writelines(f"\n \n # {datetime.now()}")
+      await log_file_object.writelines(f"\n \n {size_log_entries} # {datetime.now()}")
     
   except Exception as e:
     log.error(e)
