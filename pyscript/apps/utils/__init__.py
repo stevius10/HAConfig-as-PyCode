f11c7ef24e075f7a59cfd56725347387fa0e9186
Steven Koch <steven.johann.koch@googlemail.com>
2024-03-09 02:00:00 +0100

    auto/sync

--- pyscript/apps/utils/__init__.py
+++ pyscript/apps/utils/__init__.py
@@ -1,3 +1,5 @@
+import regex as re
+
 log_state_trigger = []
 
 def log_state_factory(expr):
@@ -5,10 +7,10 @@ def log_state_factory(expr):
   @state_trigger(expr)
   def log_state(trigger_type=None, var_name=None, value=None, old_value=None):  
     log.debug(f"[{var_name}] {trigger_type}: {value} ({old_value})")
-
-  log_state_trigger.append(log_state) 
   
-  log.info(f"Logging: {expr}")
+  log.info("+ {}".format(re.sub("and [\w]+\.[\w]+ not in \['.*'\]", "",  expr)))
+  log_state_trigger.append(log_state) 
 
+@service
 def log_state(expr):
   log_state_factory(expr)
--- pyscript/auto_default.py
+++ pyscript/auto_default.py
@@ -1,14 +1,31 @@
 from helper import expr
-import constants
+from  constants import AUTO_CONFIG_RESET_DELAY_ADGUARD, AUTO_CONFIG_RESET_DELAY_HEIZDECKE, \
+  AUTO_CONFIG_RESET_DELAY_SOFA, AUTO_CONFIG_RESET_DELAY_BETT
 
-# Küche: Heizung ausgeschalten
+default_trigger = []
+
+# Delayed default configuration
+
+entities = { 
+  "switch.adguard_home_schutz": { "default": "on", "delay": AUTO_CONFIG_RESET_DELAY_ADGUARD }, 
+  "switch.heizdecke": { "default": "off", "delay": AUTO_CONFIG_RESET_DELAY_HEIZDECKE }, 
+  "switch.sofa": { "default": "off", "delay": AUTO_CONFIG_RESET_DELAY_SOFA }, 
+  "switch.bett": { "default": "off", "delay": AUTO_CONFIG_RESET_DELAY_BETT } 
+}
+
+def on_delay_reset_factory(entity):
+  @state_trigger(expr(entity, f"!= {entities.get(entity)['delay']}"))
+  def on_delay_reset(var_name=None):
+    if entities.get(entity)['default'] == "off":
+      switch.turn_off(entity_id=var_name)
+    elif entities.get(entity)['default'] == "on":
+      switch.turn_on(entity_id=var_name)
+  return on_delay_reset
+
+for entity in entities:
+  on_delay_reset_factory(entity)
+
+# Default configuration
 @state_trigger(expr("climate.kuche", "!= 'off'", defined=True), state_check_now=True)
 def reset_climate_kuche(var_name=None):
   climate.turn_off(entity_id=var_name)
-
-# Homeassistant
-
-# Adguard angeschalten
-@state_trigger(expr("switch.adguard_home_schutz", "== 'off'"), state_hold=constants.AUTO_CONFIG_DEFAULT_RESET_DELAY, state_check_now=True)
-def reset_climate_kuche(var_name=None):
-  switch.turn_on(entity_id=var_name)
