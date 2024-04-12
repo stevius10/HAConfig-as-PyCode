595b8891818960ffbcb20d3e3d0b39260b5fa689
Steven Koch <steven.johann.koch@googlemail.com>
2024-04-12 22:49:31 +0200

    auto/sync

--- pyscript/apps/sync_git.py
+++ pyscript/apps/sync_git.py
@@ -10,27 +10,27 @@ from utils import Logfile
 import subprocess
 import requests
 
-logfile = Logfile(pyscript.get_global_ctx())
+logfile  = Logfile(pyscript.get_global_ctx())
 
 def create_or_update_pull_request(repo_owner, repo_name, base_branch, head_branch, title, body):
-    access_token =  SERVICE_GIT_GITHUB_TOKEN
-    headers = {
+    access_token  =  SERVICE_GIT_GITHUB_TOKEN
+    headers  = {
         "Authorization": f"token {access_token}",
         "Accept": "application/vnd.github.v3+json"
     }
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
-    payload = {
+    url  = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
+    payload  = {
         "title": title,
         "body": body,
         "base": base_branch,
         "head": head_branch
     }
-    response = task.executor(requests.get, url, json=payload, headers=headers)
+    response  = task.executor(requests.get, url, json=payload, headers=headers)
     if response.status_code == 201:
         return response.json()["html_url"]
     elif response.status_code == 422:
-        pull_request_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{head_branch}"
-        response = requests.patch(pull_request_url, json=payload, headers=headers)
+        pull_request_url  = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{head_branch}"
+        response  = requests.patch(pull_request_url, json=payload, headers=headers)
         if response.status_code == 200:
             return response.json()["html_url"]
     return None
@@ -38,20 +38,20 @@ def create_or_update_pull_request(repo_owner, repo_name, base_branch, head_branc
 @service(supports_response="optional")
 @time_trigger(SERVICE_GIT_CRON)
 def service_git_sync(
-  repo_url= SERVICE_GIT_REPO_URL,
-  repo_owner= SERVICE_GIT_GITHUB_USER,
-  repo_name= SERVICE_GIT_REPO_NAME,
-  base_branch= SERVICE_GIT_REPO_BASE,
-  branch_name= SERVICE_GIT_REPO_BRANCH,
-  branch_target= SERVICE_GIT_REPO_TARGET,
-  key_path= SERVICE_GIT_SETTINGS_CREDENTIALS,
-  config_path= SERVICE_GIT_SETTINGS_CONFIG,
-  commit_message= SERVICE_GIT_REPO_MESSAGE,
-  pull_request_title= SERVICE_GIT_GITHUB_PR_TITLE,
-  pull_request_body= SERVICE_GIT_GITHUB_PR_BODY
+  repo_url = SERVICE_GIT_REPO_URL,
+  repo_owner = SERVICE_GIT_GITHUB_USER,
+  repo_name = SERVICE_GIT_REPO_NAME,
+  base_branch = SERVICE_GIT_REPO_BASE,
+  branch_name = SERVICE_GIT_REPO_BRANCH,
+  branch_target = SERVICE_GIT_REPO_TARGET,
+  key_path = SERVICE_GIT_SETTINGS_CREDENTIALS,
+  config_path = SERVICE_GIT_SETTINGS_CONFIG,
+  commit_message = SERVICE_GIT_REPO_MESSAGE,
+  pull_request_title = SERVICE_GIT_GITHUB_PR_TITLE,
+  pull_request_body = SERVICE_GIT_GITHUB_PR_BODY
 ):
   
-  commands = [
+  commands  = [
       f"git config --local include.path '{config_path}'",
       f"eval $(ssh-agent); ssh-add {key_path}", 
       
@@ -79,7 +79,7 @@ def service_git_sync(
 
   for command in commands:
     try:
-      result = subprocess.run(
+      result  = subprocess.run(
           command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
           shell=True, check=False, text=True
       )
@@ -88,7 +88,7 @@ def service_git_sync(
       logfile.log([e, command, result.stdout, result.stderr])
 
   # Create or update pull request
-  pull_request_url = create_or_update_pull_request(repo_owner, repo_name, base_branch, branch_name, pull_request_title, pull_request_body)
+  pull_request_url  = create_or_update_pull_request(repo_owner, repo_name, base_branch, branch_name, pull_request_title, pull_request_body)
   if pull_request_url:
       logfile.log(f"Pull request created or updated: {pull_request_url}")
   else:
