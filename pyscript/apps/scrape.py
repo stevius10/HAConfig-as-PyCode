c0c27b2c64a595ab88e2f7ca5744092432817cc3
Steven Koch <steven.johann.koch@googlemail.com>
2024-06-19 01:15:02 +0200

    auto/sync

--- pyscript/apps/scrape.py
+++ pyscript/apps/scrape.py
@@ -17,30 +17,33 @@ housing_provider = SERVICE_SCRAPE_HOUSING_PROVIDERS
 # Function
 
 @logged
-def scrape(content, item, address_selector, area_selector, rent_selector, size_selector=None, rooms_selector=None, details_selector=None):
+def scrape(content, item, address_selector, area_selector, rent_selector, 
+           size_selector=None, rooms_selector=None, details_selector=None):
   apartments = []
   
   elements = content.select(item)
   for element in elements:
-    address = element.select_one(address_selector).get_text().strip() if element.select_one(address_selector) else None
-    area = element.select_one(area_selector).get_text().strip() if element.select_one(area_selector) else None
-    rent = element.select_one(rent_selector).get_text().strip() if element.select_one(rent_selector) else None
-    size = element.select_one(size_selector).get_text().strip() if size_selector and element.select_one(size_selector) else None
-    rooms = element.select_one(rooms_selector).get_text().strip() if rooms_selector and element.select_one(rooms_selector) else None
-    details = element.select_one(details_selector).get_text() if details_selector and element.select_one(details_selector) else None
-    apartments.append({"address": address, "area": area, "rent": rent, "size": size, "rooms": rooms, "details": details })
-
-  return apartments
+    apartment = {}
+    apartment["address"] = element.select_one(address_selector).get_text().strip() if element.select_one(address_selector) else None
+    apartment["area"] = element.select_one(area_selector).get_text().strip() if element.select_one(area_selector) else None  
+    apartment["rent"] = element.select_one(rent_selector).get_text().strip() if element.select_one(rent_selector) else None
+    apartment["size"] = element.select_one(size_selector).get_text().strip() if size_selector and element.select_one(size_selector) else None
+    apartment["rooms"] = element.select_one(rooms_selector).get_text().strip() if rooms_selector and element.select_one(rooms_selector) else None
+    apartment["details"] = element.select_one(details_selector).get_text() if details_selector and element.select_one(details_selector) else None
+    
+    apartments.append(apartment)
+      
+  return {"apartments": apartments}
 
 @pyscript_executor
 def fetch(provider, housing_provider):
-    if housing_provider[provider].get("request_headers") and housing_provider[provider].get("request_data"):
-        response = (requests.post(housing_provider[provider]["url"], headers=housing_provider[provider].get("request_headers"), data=housing_provider[provider].get("request_data"), verify=False)).text
-        content = BeautifulSoup(json.loads(response)['searchresults'], 'html.parser')
-    else:
-        response = requests.get(housing_provider[provider]["url"], verify=False)
-        content = BeautifulSoup(response.content, 'html.parser')
-    return content
+  if housing_provider[provider].get("request_headers") and housing_provider[provider].get("request_data"):
+    response = (requests.post(housing_provider[provider]["url"], headers=housing_provider[provider].get("request_headers"), data=housing_provider[provider].get("request_data"), verify=False)).text
+    content = BeautifulSoup(json.loads(response)['searchresults'], 'html.parser')
+  else:
+    response = requests.get(housing_provider[provider]["url"], verify=False)
+    content = BeautifulSoup(response.content, 'html.parser')
+  return content
   
 # Factory
 
@@ -61,11 +64,10 @@ def scrape_housing_factory(provider):
     task.sleep(random.randint(SERVICE_SCRAPE_HOUSING_DELAY_RANDOM_MIN, SERVICE_SCRAPE_HOUSING_DELAY_RANDOM_MAX))
     
     structure = housing_provider[provider]["structure"]
-    context = fetch(provider, housing_provider)
+    content = fetch(provider, housing_provider)
     state.set(entity, scrape(content, structure["item"], 
-      structure["address_selector"], structure["area_selector"], structure["rent_selector"], 
-      structure["size_selector"], structure["rooms_selector"], structure["details_selector"] ))
-    log(f"{entity} set to {state.get(entity)}", kwargs.get("context"))
+      structure["address_selector"], structure["area_selector"], structure["rent_selector"],
+      structure["size_selector"], structure["rooms_selector"], structure["details_selector"]))
     log_data(state.getattr(entity))
     
   trigger.append(scrape_housing)
