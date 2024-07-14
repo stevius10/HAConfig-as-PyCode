99e3d3874b0a4ec5164266136eef6954f0e2df9b
Steven Koch <steven.johann.koch@googlemail.com>
2024-07-14 21:55:43 +0200

    auto/sync

--- pyscript/python/logfile.py
+++ pyscript/python/logfile.py
@@ -1,17 +1,17 @@
 import json
 import logging
 import os
-import sys
 from pathlib import Path
 
-from constants.config import CFG_LOGFILE_DEBUG_FILE, CFG_LOGFILE_FORMAT, CFG_LOGFILE_LOG_SIZE, CFG_PATH_DIR_LOG
+from constants.config import CFG_LOGFILE_DEBUG_FILE, CFG_LOGFILE_FORMAT, CFG_LOGFILE_LOG_SIZE, CFG_PATH_DIR_LOG, CFG_PATH_DIR_PY_LOGS_COMPONENTS
 
 os.environ['PYTHONDONTWRITEBYTECODE'] = "1"
 
 class Logfile:
   _logger = None
 
-  def __init__(self, name=None):
+  def __init__(self, name=None, component_log=True):
+    self.component_log = component_log
     if name:
       self.name = name.split(".")[1] if not name.isalpha() else name
       self._logger = self._get_file_logger()
@@ -20,25 +20,25 @@ class Logfile:
 
   def _get_file_logger(self):
     self.history = []
-    self.logfile = Path(CFG_PATH_DIR_LOG, f"{self.name}.log")
-    logger = self._create_logger(name=self.name)
+    log_dir = CFG_PATH_DIR_PY_LOGS_COMPONENTS if self.component_log else CFG_PATH_DIR_LOG
+    self.logfile = Path(log_dir, f"{self.name}.log")
+    logger = self._create_logger(self.logfile)
     return logger
 
   @classmethod
   def _get_debug_logger(cls):
     if cls._logger is None:
-      cls._logger = cls._create_logger(name=CFG_LOGFILE_DEBUG_FILE)
+      debug_logfile = Path(CFG_PATH_DIR_LOG, f"{CFG_LOGFILE_DEBUG_FILE}.log")
+      cls._logger = cls._create_logger(debug_logfile)
     return cls._logger
 
   @staticmethod
-  def _create_logger(name):
-    logfile = Path(CFG_PATH_DIR_LOG, f"{name}.log")
-    logger = logging.getLogger(name)
+  def _create_logger(logfile):
+    logger = logging.getLogger(logfile.stem)
     if logger.hasHandlers():
       logger.handlers.clear()
     handler = logging.FileHandler(logfile, mode='w+')
     handler.setFormatter(logging.Formatter(CFG_LOGFILE_FORMAT))
-    logger = logging.getLogger(name)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
@@ -81,6 +81,6 @@ class Logfile:
           self.history = lines_half_start + [f"... [{lines - len(lines_half_start) - len(lines_half_stop)} lines] ..."] + lines_half_stop
         self.history = " ".join(self.history)
 
-      return { "file": Path(CFG_PATH_DIR_LOG, f"{self.name}.log").as_posix(), "result": self.history }
+      return { "file": self.logfile.as_posix(), "result": self.history }
     except Exception as e:
       return str(e)
--- pyscript/scripts/ha_system.py
+++ pyscript/scripts/ha_system.py
@@ -1,32 +1,25 @@
 import os
 import sys
+from collections import defaultdict
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
-
-from constants.config import CFG_PATH_DIR_PY, CFG_EVENT_STARTED_DELAY, CFG_SYSTEM_ENVIRONMENT, CFG_SYSTEM_FILES, CFG_SYSTEM_LINKS
+from constants.config import CFG_EVENT_STARTED_DELAY, CFG_LOG_SETTINGS_ENVIRONMEMT_LENGTH, CFG_PATH_DIR_PY, CFG_SYSTEM_ENVIRONMENT, CFG_SYSTEM_FILES, CFG_SYSTEM_LINKS
 from constants.mappings import MAP_EVENT_SETUP_STARTED, MAP_EVENT_SYSTEM_STARTED
-
 from utils import *
 
 trigger = []
 
-# Initialization
-
-# @pyscript_executor # code redundancy suppress to pass code runtime wise
-# def ha_setup_init_native(path=CFG_PATH_DIR_PY):
-#   os.environ['PYTHONDONTWRITEBYTECODE'] = "1"
-#   sys.path.extend([path for path in [os.path.join(CFG_PATH_DIR_PY, subdir) for subdir in os.listdir(CFG_PATH_DIR_PY) if os.path.isdir(os.path.join(CFG_PATH_DIR_PY, subdir))] if path not in sys.path])
-#   return [path for path in sys.path]
+# Startup
 
 @time_trigger
 @event_trigger(EVENT_HOMEASSISTANT_STARTED)
 def event_setup_init(delay=CFG_EVENT_STARTED_DELAY):
   os.environ['PYTHONDONTWRITEBYTECODE'] = "1"
-  sys.path.extend([path for path in [os.path.join(CFG_PATH_DIR_PY, subdir) for subdir in os.listdir(CFG_PATH_DIR_PY) if os.path.isdir(os.path.join(CFG_PATH_DIR_PY, subdir))] if path not in sys.path])
-  # ha_setup_init_native()
-  
+  sys.path.extend([
+    path for path in [os.path.join(CFG_PATH_DIR_PY, subdir) for subdir in os.listdir(CFG_PATH_DIR_PY) if os.path.isdir(os.path.join(CFG_PATH_DIR_PY, subdir))] 
+    if path not in sys.path
+  ])
   task.sleep(delay)
   event.fire(MAP_EVENT_SETUP_STARTED)
-  return None
 
 # Setup
 
@@ -39,26 +32,55 @@ def ha_setup():
   event.fire(MAP_EVENT_SYSTEM_STARTED)
 
 # Tasks
-  
-@logged
-def ha_setup_environment(variables=CFG_SYSTEM_ENVIRONMENT):
-  for variable, value in variables.items():
-    os.environ[variable] = value
-  return ", ".join([f"{key}: {value}" for key, value in os.environ.items()]).replace(': ', ":")
 
 @logged
-# @pyscript_executor
+def ha_setup_environment(variables=CFG_SYSTEM_ENVIRONMENT):
+  def shorten(val, max_len=CFG_LOG_SETTINGS_ENVIRONMEMT_LENGTH):
+    return val if len(val) <= max_len else val[:max_len] + '..'
+  
+  os.environ.update({k: v for k, v in variables.items() if not (k.startswith('S6') or k.startswith('__'))})
+  env_vars = defaultdict(list)
+  single_vars = []
+  
+  for k in sorted(os.environ.keys()):
+    if not (k.startswith('S6') or k.startswith('__')):
+      env_vars[k[0]].append(f"{k}={shorten(os.environ[k])}")
+  
+  for key, vals in list(env_vars.items()):
+    if len(vals) == 1:
+      single_vars.extend(vals)
+      del env_vars[key]
+
+  if single_vars:
+    for i in range(0, len(single_vars), 2):
+      pair = single_vars[i:i + 2]
+      env_vars["A"].append(", ".join(pair))
+  
+  formatted_env_vars = "\n  ".join([", ".join(vals) for vals in env_vars.values()])
+  return f"Environment:\n  {formatted_env_vars}"
+
+@logged
 def ha_setup_files(files=CFG_SYSTEM_FILES):
   from filesystem import cp
   for src, dest in files.items():
     cp(src, dest)
-  return ", ".join([path for path in sys.path]).replace(': ', ":")
+  
+  paths = defaultdict(list)
+  for path in sorted(set([f"{os.sep.join(path.split(os.sep)[:4])}/…" if len(path.split(os.sep)) > 4 else path for path in sys.path if not path.startswith('__')])):
+    key = os.sep.join(path.split(os.sep)[:2]) if len(path.split(os.sep)) > 1 else path
+    paths[key].append(path)
+  
+  formatted_paths = "\n  ".join([", ".join(vals) for key, vals in paths.items() if key.startswith('/config') and not key.startswith('/config/pyscript')])
+  formatted_paths += "\n  " + ", ".join([", ".join(vals) for key, vals in paths.items() if key.startswith('/config/pyscript')])
+  formatted_paths += "\n  " + ", ".join([", ".join(vals) for key, vals in paths.items() if not key.startswith('/config')])
+  
+  return f"System Path:\n  {formatted_paths}"
 
 @logged
-# @pyscript_executor
 def ha_setup_links(links=CFG_SYSTEM_LINKS):
-  for source, target in links.items():
-    if not os.path.isdir(target) and os.path.islink(target):
-      os.unlink(target)
-    os.symlink(source, target)
-  return ", ".join([f"{source} <- {target}" for source, target in links.items()]).replace(': ', ":")
+  for src, dest in links.items():
+    if not os.path.isdir(dest) and os.path.islink(dest):
+      os.unlink(dest)
+    os.symlink(src, dest)
+  formatted_links = "\n".join([f"{src} <- {dest}" for src, dest in links.items()])
+  return f"Sym. Links:\n{formatted_links}"
