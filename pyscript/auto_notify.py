ddc730116a28d24f7bb8fee51f7c2ef671ea01a2
Steven Koch <steven.johann.koch@googlemail.com>
2024-08-22 01:30:02 +0200

    auto/sync

--- pyscript/auto_notify.py
+++ pyscript/auto_notify.py
@@ -9,9 +9,7 @@ from utils import *
 @state_trigger(expr(entity=[name for name in state.names() if MAP_PERSISTENCE_PREFIX_SCRAPE_HOUSING in name]))
 @logged
 def notify_housing(target=CFG_NOTIFICATION_TARGET_DEFAULT, default=True, var_name=None, value=None, old_value=None):
-  if value and value not in MAP_STATE_HA_UNDEFINED:
-    diff = compare(old_value, value)
-  if diff:
+  if value and value not in MAP_STATE_HA_UNDEFINED and compare(old_value, value):
     url = DATA_SCRAPE_HOUSING_PROVIDERS.get(var_name.split(f"{MAP_PERSISTENCE_PREFIX_SCRAPE_HOUSING}_")[1]).get("url")
     apartments = ", ".join(str(apartment) for apartment in [Apartment(**apartment_data) for apartment_data in value.get("value", [])])
     pyscript.shortcut(message=f"{var_name}: {apartments}", shortcut=MAP_SERVICE_SCRAPE_HOUSING_SHORTCUT_NAME, input=url)
