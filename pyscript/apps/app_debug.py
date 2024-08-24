fcb4a78953c2842abe327ba5f5cf0ca76406e24a
Steven Koch <steven.johann.koch@googlemail.com>
2024-08-25 01:30:00 +0200

    auto/sync

--- pyscript/apps/app_debug.py
+++ pyscript/apps/app_debug.py
@@ -1,33 +1,39 @@
+import subprocess
+import requests
+import sys
+import pkgutil
+import importlib
+
 from constants.expressions import *
 from constants.secrets import *
 from constants.settings import *
 
-import subprocess
-import requests
-
 from utils import *
+
+def get_logfile_name(suffix):
+  return f"{pyscript.get_global_ctx().split('.')[1]}.debug.{suffix}"
 
 @logged
 @service(supports_response="optional")
-def debug():
+def debug(): 
   return dict(debug_function())
- 
+
 @pyscript_executor
 def debug_function():
-  from logfile import Logfile # runtime level due sys path config
-  logfile  = Logfile("debug.services")
-  
-  import sys
-  import pkgutil
-  import importlib
-  
-  logfile.log("Loaded modules:")
-  for module_name in sorted(sys.modules.keys()):
-    logfile.log(module_name)
-  
-  logfile.log("\nAvailable modules in the environment:")
-  available_modules = sorted([module.name for module in pkgutil.iter_modules()])
-  for module_name in available_modules:
-    logfile.log(module_name)
+  from logfile import Logfile
+  Logfile(get_logfile_name("log")).log(" ".join(sorted(sys.modules.keys()))).log(" ".join([module.name for module in pkgutil.iter_modules()])).close()
 
-  return logfile.close()
+@event_trigger("*")
+def debug_events(service_data=None, **kwargs):
+  from logfile import Logfile
+  Logfile(get_logfile_name("events")).log(f"{pyscript.event_name()}: {service_data} ({kwargs})").close()
+
+@time_trigger
+@service
+def debug_triggers():
+  triggers = {}
+  for ctx_name, ctx in pyscript.get_global_ctx().items():
+    ctx_triggers = ctx.get_triggers()
+    if ctx_triggers:
+      triggers[ctx_name] = [str(trig) for trig in ctx_triggers]
+  return {"triggers": triggers}
