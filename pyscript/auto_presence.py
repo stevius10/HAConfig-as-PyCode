2c2b551f19e810235f9f10133cf9f3da1ed7354e
Steven Koch <steven.johann.koch@googlemail.com>
2024-11-22 01:30:00 +0100

    auto/sync

--- pyscript/auto_presence.py
+++ pyscript/auto_presence.py
@@ -1,37 +1,40 @@
 from constants.data import DATA_PRESENCE
 from constants.entities import ENTITIES_PRESENCE
-from constants.mappings import MAP_EVENT_SYSTEM_STARTED, MAP_PERSISTENCE_ENTITY_PRESENCE
-
+from constants.mappings import MAP_PERSISTENCE_ENTITY_PRESENCE
 from utils import *
 
 trigger = []
 
 def presence_factory(room):
-
-  @event_trigger(MAP_EVENT_SYSTEM_STARTED)
-  def presence_init():
-    store(MAP_PERSISTENCE_ENTITY_PRESENCE)
-
-  trigger_conditions = [expr(entity, condition['condition']) for entity, condition in ENTITIES_PRESENCE[room]['indicators'].items()]
-  # @state_trigger(trigger_conditions)
+  
+  trigger_conditions = [f"state.{entity} == '{condition['condition']}'" for entity, condition in ENTITIES_PRESENCE[room]['indicators'].items()]
+  @state_trigger(trigger_conditions)
   @logged
   def presence(var_name=None):
-    indicators_weight = sum([condition.get('weight', 1) for entity, condition in ENTITIES_PRESENCE[room]['indicators'].items() if state.get(entity) == condition['condition']])
-    exclusions_weight = sum([condition.get('weight', 1) for entity, condition in ENTITIES_PRESENCE[room]['exclusions'].items() if state.get(entity) == condition['condition']])
+    indicators_weight = sum(condition.get('weight', 1) for entity, condition in ENTITIES_PRESENCE[room]['indicators'].items() if state.get(entity) == condition['condition'])
+    exclusions_weight = sum(condition.get('weight', 1) for entity, condition in ENTITIES_PRESENCE[room]['exclusions'].items() if state.get(entity) == condition['condition'])
     if indicators_weight >= 1 and exclusions_weight == 0:
       update_presence(room, 'on')
     else:
       update_presence(room, 'off')
   trigger.append(presence)
 
-def update_presence(room, action, entity=MAP_PERSISTENCE_ENTITY_PRESENCE):
-  if entity: 
-    current_state = state.get(entity) if state.get(entity) else {}
-    attributes = current_state.get('attributes', {}) if current_state else {}
-    attributes[room] = action
-    global_state = 'on' if any(status == 'on' for status in attributes.values()) else 'off'
-    store(entity, global_state, attributes=attributes)
+  @time_trigger('startup')
+  def presence_init():
+    existing_attributes = state.get(f"{MAP_PERSISTENCE_ENTITY_PRESENCE}", {}).get('attributes', {})
+    initial_attributes = {room: existing_attributes.get(room, 'off') for room in ENTITIES_PRESENCE}
+    state.persist(f"{MAP_PERSISTENCE_ENTITY_PRESENCE}", default_value="", attributes=initial_attributes)
+    homeassistant.update_entity(entity_id=MAP_PERSISTENCE_ENTITY_PRESENCE)
 
+def update_presence(room, action):
+  current_state = state.get(f"{MAP_PERSISTENCE_ENTITY_PRESENCE}", {})
+  attributes = current_state.get('attributes', {}) if current_state else {}
+  attributes[room] = action
+  global_state = 'on' if any(status == 'on' for status in attributes.values()) else 'off'
+  state.set(f"{MAP_PERSISTENCE_ENTITY_PRESENCE}", global_state, attributes=attributes)
+  homeassistant.update_entity(entity_id=MAP_PERSISTENCE_ENTITY_PRESENCE)
+  state.persist(f"{MAP_PERSISTENCE_ENTITY_PRESENCE}")
+  
   for transition in DATA_PRESENCE.get(room, {}).get(action, []):
     if eval(transition['condition']):
       transition['action']()
